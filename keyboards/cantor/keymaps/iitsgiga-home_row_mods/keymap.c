eeae4eff85a8981170d0f6621ebffb9e0159ff82
Manuel-Alexander Oppermann <manuel.alexander.oppermann@gmail.com>
2023-10-14 00:29:54 +0200

    make keymap compatible with default qwertz

--- keyboards/cantor/keymaps/iitsgiga-home_row_mods/keymap.c
+++ keyboards/cantor/keymaps/iitsgiga-home_row_mods/keymap.c
@@ -2,88 +2,71 @@
 #include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
-#include "keymap_us_international_linux.h"
+#include "keymap_german.h"
 
-#define _BASE_LETTERS 0
-#define _BASE_NAV 1
-#define _MOUSE_NAV 2
-#define _MEDIA_NAV 3
-#define _BASE_NUMS 4
-#define _BASE_SYMS 5
-#define _ALT_SYMS 6
-#define _FUNC_NAV 7
-
-#define MY_QUOT US_ACUT
-#define MY_DQUO S(US_ACUT)
-#define MY_ACUT ALGR(US_QUOT)
-#define MY_DIAE S(ALGR(US_QUOT))
-#define MY_GRV US_DGRV
-#define MY_TILD S(US_DGRV)
-#define MY_CIRC S(US_6)
+enum Layers {
+    BASE_LETTERS = 0,
+    BASE_NAV,
+    MOUSE_NAV,
+    MEDIA_NAV,
+    BASE_NUMS,
+    BASE_SYMS,
+    ALT_SYMS,
+    FUNC_NAV,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Dvorak: */
-    /*
-    [_BASE_LETTERS] = LAYOUT_split_3x6_3(
-        XXXXXXX, US_ADIA,      US_ODIA,      US_SS,        US_P,         US_Y,             US_F, US_G,         US_C,         US_R,         US_L,         XXXXXXX,
-        XXXXXXX, LGUI_T(US_A), LALT_T(US_O), LSFT_T(US_E), LCTL_T(US_U), US_I,             US_D, LCTL_T(US_H), LSFT_T(US_T), LALT_T(US_N), LGUI_T(US_S), XXXXXXX,
-        XXXXXXX, US_UDIA,      US_Q,         US_J,         US_K,         US_X,             US_B, US_M,         US_W,         US_V,         US_Z,         XXXXXXX,
-             LT(_FUNC_NAV, KC_ESC), LT(_BASE_NUMS, KC_SPC), LT(_BASE_NAV, KC_TAB),     LT(_MOUSE_NAV, KC_ENT), LT(_BASE_SYMS, US_DOT), LT(_MEDIA_NAV, US_COMM)
-    ),
-    */
-
-    /* Bone: */
-    [_BASE_LETTERS] = LAYOUT_split_3x6_3(
-        XXXXXXX, US_J,         US_D,         US_U,         US_A,         US_X,                US_P, US_H,         US_L,         US_M,         US_W,         XXXXXXX,
-        XXXXXXX, LGUI_T(US_C), LALT_T(US_T), LSFT_T(US_I), LCTL_T(US_E), US_O,                US_B, LCTL_T(US_N), LSFT_T(US_R), LALT_T(US_S), LGUI_T(US_G), XXXXXXX,
-        XXXXXXX, US_F,         US_V,         US_UDIA,      US_ADIA,      US_ODIA,             US_Y, US_Z,         US_Q,         US_SS,        US_K,         XXXXXXX,
-               LT(_FUNC_NAV, KC_ESC), LT(_BASE_NUMS, KC_SPC), LT(_MOUSE_NAV, KC_TAB),     LT(_BASE_NAV, US_DOT), LT(_BASE_SYMS, KC_ENT), LT(_MEDIA_NAV, US_COMM)
+    [BASE_LETTERS] = LAYOUT_split_3x6_3(
+        XXXXXXX, DE_J,         DE_D,         DE_U,         DE_A,         DE_X,                DE_P, DE_H,         DE_L,         DE_M,         DE_W,         XXXXXXX,
+        XXXXXXX, LGUI_T(DE_C), LALT_T(DE_T), LSFT_T(DE_I), LCTL_T(DE_E), DE_O,                DE_B, LCTL_T(DE_N), LSFT_T(DE_R), LALT_T(DE_S), LGUI_T(DE_G), XXXXXXX,
+        XXXXXXX, DE_F,         DE_V,         DE_UDIA,      DE_ADIA,      DE_ODIA,             DE_Y, DE_Z,         DE_Q,         DE_SS,        DE_K,         XXXXXXX,
+                 LT(FUNC_NAV, KC_ESC), LT(BASE_NUMS, KC_SPC), LT(MOUSE_NAV, KC_TAB),     LT(BASE_NAV, DE_DOT), LT(BASE_SYMS, KC_ENT), LT(MEDIA_NAV, DE_COMM)
     ),
 
-    [_BASE_NAV] = LAYOUT_split_3x6_3(
+    [BASE_NAV] = LAYOUT_split_3x6_3(
         XXXXXXX, KC_BSPC, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX,             XXXXXXX, KC_BSPC, KC_UP,   KC_DEL,  KC_PGUP, XXXXXXX,
         XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,             KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, KC_HOME, KC_INS,  KC_END,  CW_TOGG, XXXXXXX,
-                                       XXXXXXX, XXXXXXX, _______,     XXXXXXX, XXXXXXX, XXXXXXX
-    ),
-
-    [_MOUSE_NAV] = LAYOUT_split_3x6_3(
-        XXXXXXX, KC_ACL2, KC_BTN2, KC_MS_U, KC_BTN1, XXXXXXX,             XXXXXXX, KC_BTN5, KC_WH_U, KC_BTN4, XXXXXXX, XXXXXXX,
-        XXXXXXX, KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,             XXXXXXX, KC_WH_D, KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX,
-        XXXXXXX, KC_ACL0, KC_BTN5, KC_BTN3, KC_BTN4, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                        XXXXXXX, XXXXXXX, XXXXXXX,     _______, XXXXXXX, XXXXXXX
     ),
 
-    [_MEDIA_NAV] = LAYOUT_split_3x6_3(
+    [MOUSE_NAV] = LAYOUT_split_3x6_3(
+        XXXXXXX, KC_ACL2, KC_BTN2, KC_MS_U, KC_BTN1, XXXXXXX,             XXXXXXX, KC_BTN5, KC_WH_U, KC_BTN4, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,             XXXXXXX, KC_WH_D, KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_ACL0, KC_BTN5, KC_BTN3, KC_BTN4, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                       XXXXXXX, XXXXXXX, _______,     XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    [MEDIA_NAV] = LAYOUT_split_3x6_3(
         XXXXXXX, QK_RBT,  XXXXXXX, KC_BRIU, XXXXXXX, XXXXXXX,             XXXXXXX, KC_MSTP, KC_VOLU, KC_MPLY, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID, XXXXXXX, XXXXXXX,             XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT, XXXXXXX, XXXXXXX,
         XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX,
                                        XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, _______
     ),
 
-    [_BASE_NUMS] = LAYOUT_split_3x6_3(
-        XXXXXXX, US_A,         US_B,         US_C,         US_F,           US_D,                US_O, US_7,         US_8,         US_9, MY_QUOT, XXXXXXX,
-        XXXXXXX, LGUI_T(US_D), LALT_T(US_E), LSFT_T(US_F), LCTL_T(US_DOT), US_COMM,             US_X, LCTL_T(US_4), LSFT_T(US_5), US_6, US_UNDS, XXXXXXX,
-        XXXXXXX, XXXXXXX,      XXXXXXX,      US_E,         US_PLUS,        US_MINS,             US_B, US_1,         US_2,         US_3, US_DOT,  XXXXXXX,
-                                                             XXXXXXX, _______, XXXXXXX,     XXXXXXX, US_0, XXXXXXX
+    [BASE_NUMS] = LAYOUT_split_3x6_3(
+        XXXXXXX, DE_A,         DE_B,         DE_C,         DE_F,           DE_D,                DE_O, DE_7,         DE_8,         DE_9, DE_QUOT, XXXXXXX,
+        XXXXXXX, LGUI_T(DE_D), LALT_T(DE_E), LSFT_T(DE_F), LCTL_T(DE_DOT), DE_COMM,             DE_X, LCTL_T(DE_4), LSFT_T(DE_5), DE_6, DE_UNDS, XXXXXXX,
+        XXXXXXX, XXXXXXX,      XXXXXXX,      DE_E,         DE_PLUS,        DE_MINS,             DE_B, DE_1,         DE_2,         DE_3, DE_DOT,  XXXXXXX,
+                                                             XXXXXXX, _______, XXXXXXX,     XXXXXXX, DE_0, XXXXXXX
     ),
 
-    [_BASE_SYMS] = LAYOUT_split_3x6_3(
-        XXXXXXX, US_AT,   US_LABK, US_RABK, US_ASTR, US_SLSH,             MY_DQUO, US_QUES, US_LCBR, US_RCBR, US_SCLN, XXXXXXX,
-        XXXXXXX, US_EXLM, US_LPRN, US_RPRN, US_PLUS, US_MINS,             MY_QUOT, US_EQL,  US_LBRC, US_RBRC, US_COLN, XXXXXXX,
-        XXXXXXX, MY_TILD, US_PIPE, US_AMPR, MY_CIRC, US_PERC,             MY_GRV,  US_HASH, US_DLR,  US_EURO, US_BSLS, XXXXXXX,
-                                 XXXXXXX, US_UNDS, MO(_ALT_SYMS),     XXXXXXX, _______, XXXXXXX
+    [BASE_SYMS] = LAYOUT_split_3x6_3(
+        XXXXXXX, DE_AT,   DE_LABK, DE_RABK, DE_ASTR, DE_SLSH,             DE_DQUO, DE_QUES, DE_LCBR, DE_RCBR, DE_SCLN, XXXXXXX,
+        XXXXXXX, DE_EXLM, DE_LPRN, DE_RPRN, DE_PLUS, DE_MINS,             DE_QUOT, DE_EQL,  DE_LBRC, DE_RBRC, DE_COLN, XXXXXXX,
+        XXXXXXX, DE_TILD, DE_PIPE, DE_AMPR, DE_CIRC, DE_PERC,             DE_GRV,  DE_HASH, DE_DLR,  DE_EURO, DE_BSLS, XXXXXXX,
+                                 XXXXXXX, DE_UNDS, MO(ALT_SYMS),     XXXXXXX, _______, XXXXXXX
     ),
 
-    [_ALT_SYMS] = LAYOUT_split_3x6_3(
-        XXXXXXX, US_COPY, US_LDAQ, US_RDAQ, US_MUL,  US_DIV,              US_ETH,  US_IQUE, US_LSQU, US_RSQU, US_CURR, XXXXXXX,
-        XXXXXXX, US_IEXL, US_LDQU, US_RDQU, US_OSTR, US_MICR,             US_SECT, US_QRTR, US_HALF, US_TQTR, US_PND,  XXXXXXX,
-        XXXXXXX, US_NOT,  US_BRKP, US_REGD, US_DEG,  US_CENT,             MY_ACUT, US_SUP1, US_SUP2, US_SUP3, US_YEN,  XXXXXXX,
+    [ALT_SYMS] = LAYOUT_split_3x6_3(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DE_MICR,             DE_SECT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DE_DEG,  XXXXXXX,             DE_ACUT, XXXXXXX, DE_SUP2, DE_SUP3, XXXXXXX, XXXXXXX,
                                        XXXXXXX, XXXXXXX, _______,     XXXXXXX, _______, XXXXXXX
     ),
 
-    [_FUNC_NAV] = LAYOUT_split_3x6_3(
+    [FUNC_NAV] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_PSCR, KC_F7, KC_F8, KC_F9, KC_F10, XXXXXXX,
         XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,             KC_SCRL, KC_F4, KC_F5, KC_F6, KC_F11, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             KC_PAUS, KC_F1, KC_F2, KC_F3, KC_F12, XXXXXXX,
--- keyboards/cantor/keymaps/iitsgiga-home_row_mods/readme.md
+++ keyboards/cantor/keymaps/iitsgiga-home_row_mods/readme.md
@@ -1,17 +1,13 @@
 # Cantor Remix keymap created by iiTsGiga 
 
-This is a custom keymap for the *[Cantor Remix](https://keebd.com/products/cantor-remix-keyboard-kit)* keyboard from *[keebd.com](https://keebd.com/)* inspired by the *[Miryoku](https://github.com/manna-harbour/miryoku/)* keymap.
+This is a custom keymap for the *[Cantor Remix](https://keebd.com/products/cantor-remix-keyboard-kit)* keyboard from *[keebd.com](https://keebd.com/)*.
 
-Changes include:
+Features include:
 
-- ~Dvorak layout on base layer (including umlauts and eszett)~
 - modified *[Bone](https://www.neo-layout.org/Layouts/bone/)* layout on base layer
-- relocation of keys on every layer
-- removed button and additional features layers (added quantum keys to media layer)
-- replaced modifiers on symbol layer with additional symbols and added second symbol layer for US international layout specific characters
-- removed modifiers from media and mouse layers
-- expanded number layer with additional characters to allow for scientific and programming notations
-
-## Requirements
-
-- [US International Layout (no dead keys) for Windows](https://github.com/umanovskis/win-kbd-usint-nodead/)
+- home row modifier keys
+- number layer with additional characters to allow for scientific and programming notations
+- separate symbol layers
+- function key layer
+- mouse input layer
+- media key layer (with quantum keys for rebooting and bootloader mode)
